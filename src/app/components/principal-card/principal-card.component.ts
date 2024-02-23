1ba152794ed00c3f72081b296c77209157b70cba
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-02-23 15:48:13 -0300

    feat: Finalizando a estrutura do card principal

--- src/app/components/principal-card/principal-card.component.ts
+++ src/app/components/principal-card/principal-card.component.ts
@@ -1,9 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-principal-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './principal-card.component.html',
   styleUrl: './principal-card.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TitleMenuComponent } from "../../components/title-menu/title-menu.component";
+import { PrincipalCardComponent } from "../../components/principal-card/principal-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [TitleMenuComponent]
+  imports: [TitleMenuComponent, PrincipalCardComponent]
 })
 export class HomeComponent {
 
