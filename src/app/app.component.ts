c65a271bc21e44ea58c64942a6eb5709dbec81cb
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-02-23 13:01:38 -0300

    feat: Realizando o Title da aplicação e a estilização dele

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent } from "./components/nav-menu/nav-menu.component";
+import { TitleMenuComponent } from "./components/title-menu/title-menu.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavMenuComponent]
+  imports: [RouterOutlet, NavMenuComponent, TitleMenuComponent]
 })
 export class AppComponent {
   title = 'angular-blog-dio';
